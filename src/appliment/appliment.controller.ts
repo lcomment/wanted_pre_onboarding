5adc103218b19b9c513a6116b9d08c84ecc50da1
khsexk <kohyunsuk98@gmail.com>
2022-06-14 19:41:38 +0900

    #8 api 명세

--- src/appliment/appliment.controller.ts
+++ src/appliment/appliment.controller.ts
@@ -1,7 +1,7 @@
 import { Post } from '@nestjs/common';
 import { Body } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ApplimentService } from './appliment.service';
 
 @ApiTags('채용 공고 지원')
@@ -9,6 +9,43 @@ import { ApplimentService } from './appliment.service';
 export class ApplimentController {
     constructor(private applimentService: ApplimentService) {}
 
+    @ApiBody({
+        description: '유저와 채용공고 id',
+        schema: {
+            example: {
+                userId: 5,
+                postId: 6,
+            }
+        }
+    })
+    @ApiResponse({
+        description: 'Fail (잘못된 채용공고 id 또는 유저 id이거나 이미 지원한 경우)',
+        status: 400,
+        schema: {
+            example: {
+                success: false,
+                errorMsg: {
+                    statusCode: 400,
+                    message: 'Bad Request'
+                }
+            }
+        }
+    })
+    @ApiResponse({
+        description: 'Success',
+        status: 201,
+        schema: {
+            example: {
+                success: true,
+                ApplyUser: {
+                    postid: 6,
+                    userid: 5,
+                    email: 'kwon@wanted.com',
+                }
+            }
+        }
+    })
+    @ApiOperation({ summary: '요구사항6: 채용공고 지원하기'})
     @Post('')
     async applyPost(
         @Body('postId') postId: number,
